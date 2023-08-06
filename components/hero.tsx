2a0671712fe72b644f2aaa940b2190f9335371c9
AhmadBShaik <ahmad.shaik1106@gmail.com>
2023-08-06 17:11:15 +0530

    feat:added typewriter animation in landing page

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,8 +1,14 @@
 import { useSectionContext } from "@/context/sectionContext";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 export const Hero = ({ homeRef }: { homeRef: React.MutableRefObject<HTMLElement | null> }) => {
   const { sectionName } = useSectionContext();
 
+  const [text, helper] = useTypewriter({
+    words: ["TypeScript.", "React.js.", "Next.js.", "Tailwind.css."],
+    loop: false,
+  })
+  const { isType, isDelete, isDelay, isDone } = helper
   return <section
     className={`w-full h-screen -mt-16 sm:-mt-20 flex items-center ${sectionName === "/Projects"
       ? "bg-white"
@@ -14,13 +20,20 @@ export const Hero = ({ homeRef }: { homeRef: React.MutableRefObject<HTMLElement 
       <div className="flex px-5 md:px-0">
         <div className="sm:w-1/6"></div>
         <div className="sm:w-5/6">
-          <div className="font-bold text-white">Hi there!</div>
-          <div className="font-bold text-white text-3xl md:text-4xl xl:text-6xl">
+          <div className="font-bold text-white text-2xl md:text-4xl">Hi there! 👋</div>
+          <div className="font-bold text-white text-4xl md:text-6xl xl:text-6xl">
             I'm <span className="text-teal-300">Ahmad</span>
           </div>
-          <div className="font-bold text-white">
-            a passionate Frontend/React Developer.
+          <div className="font-bold text-white text-md md:text-2xl mt-2">
+            a passionate Software Developer.
           </div>
+          <span className={`font-bold text-white text-md md:text-2xl mt-0.5`}>
+            I write <span className="text-teal-200">{text}</span>
+          </span>
+          {isType}
+          <span className="text-white">
+            <Cursor />
+          </span>
         </div>
       </div>
     </div>
