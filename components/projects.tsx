bfc5d1fc08df49c877bf82d51fc9675083e320a9
AhmadBShaik <ahmad.shaik1106@gmail.com>
2023-08-26 22:52:49 +0530

    update skills, resume

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,4 +1,10 @@
-export const Projects = ({ projectsRef }: { projectsRef: React.MutableRefObject<HTMLElement | null> }) => {
+import { Link } from "react-scroll"
+
+export const Projects = ({
+  projectsRef,
+}: {
+  projectsRef: React.MutableRefObject<HTMLElement | null>
+}) => {
   const projects = [
     {
       title: "Happy Travel",
@@ -10,16 +16,17 @@ export const Projects = ({ projectsRef }: { projectsRef: React.MutableRefObject<
     {
       title: "My portfolio",
       description:
-        "This portfolio is created by me, using Next.js, React.js and Tailwind.css inspired by serveral curated portfolios by emma bostian https://github.com/emmabostian/developer-portfolios",
+        "This portfolio is created by me, using Next.js, React.js and Tailwind.css.",
       sourceCode: "https://github.com/AhmadBShaik/portfolio/",
-      viewLive: "https://ahmad-portfolio-nine.vercel.app/",
+      viewLive: "#",
+      noRedirect: true,
     },
     {
       title: "Ethereum unit converter",
       description:
         "The converter which is used to convert ethereum units. Converter which contains simple and extended sections. This application is created from scratch without any dependent library. Used string manipulation for managing large numbers.",
       sourceCode: "https://github.com/AhmadBShaik/EthereumUnitConverter",
-      viewLive: "https://ahmadbshaik.github.io/ethereum-unit-converter/"
+      viewLive: "https://ahmadbshaik.github.io/ethereum-unit-converter/",
     },
     {
       title: "Task Manager with Blockchain",
@@ -47,41 +54,54 @@ export const Projects = ({ projectsRef }: { projectsRef: React.MutableRefObject<
       sourceCode: "https://github.com/AhmadBShaik/StayInspired",
       viewLive: "https://ahmadbshaik.github.io/stay-inspired/",
     },
-  ];
-  return <section
-    className="w-full max-w-7xl mx-auto p-5 min-h-[calc(100vh-64px)] sm:min-h-[calc(100vh-80px)]"
-    ref={projectsRef}
-    id="projects">
-    <div className="my-5 text-xl font-bold">
-      Personal Projects
-    </div>
-    <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 cursor-default">
-      {projects.map((project) => (
-        <div
-          className="border p-5 rounded shadow-md flex flex-col"
-          key={project.title}>
-          <p className="text-lg font-medium">{project.title}</p>
-          <p className="flex-1 mt-2.5">{project.description}</p>
-          <div className="flex justify-between mt-2.5">
-            <div
-              className="text-emerald-500 hover:text-emerald-700 bg-white hover:bg-emerald-50 px-2 py-1 rounded cursor-pointer border border-emerald-500 hover:border-emerald-700"
-              onClick={() => {
-                window.open(project.sourceCode, "_blank");
-              }}>
-              Source Code
-            </div>
-            {project.viewLive ? (
+  ]
+  return (
+    <section
+      className="w-full max-w-7xl mx-auto p-5 min-h-[calc(100vh-64px)] sm:min-h-[calc(100vh-80px)]"
+      ref={projectsRef}
+      id="projects">
+      <div className="my-5 text-xl font-bold">Personal Projects</div>
+      <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 cursor-default">
+        {projects.map((project) => (
+          <div
+            className="border p-5 rounded shadow-md flex flex-col"
+            key={project.title}>
+            <p className="text-lg font-medium">{project.title}</p>
+            <p className="flex-1 mt-2.5">{project.description}</p>
+            <div className="flex justify-between mt-2.5">
               <div
-                className="bg-emerald-500 hover:bg-emerald-700 text-white px-2 py-1 rounded cursor-pointer"
+                className="text-emerald-500 hover:text-emerald-700 bg-white hover:bg-emerald-50 px-2 py-1 rounded cursor-pointer border border-emerald-500 hover:border-emerald-700"
                 onClick={() => {
-                  window.open(project.viewLive, "_blank");
+                  window.open(project.sourceCode, "_blank")
                 }}>
-                View Live
+                Source Code
               </div>
-            ) : null}
+              {project.viewLive ? (
+                project.noRedirect ? (
+                  <Link
+                    activeClass="active"
+                    to="hero"
+                    spy={true}
+                    smooth={true}
+                    duration={500}>
+                    <div className="bg-emerald-500 hover:bg-emerald-700 text-white px-2 py-1 rounded cursor-pointer">
+                      View Live
+                    </div>
+                  </Link>
+                ) : (
+                  <div
+                    className="bg-emerald-500 hover:bg-emerald-700 text-white px-2 py-1 rounded cursor-pointer"
+                    onClick={() => {
+                      window.open(project.viewLive, "_blank")
+                    }}>
+                    View Live
+                  </div>
+                )
+              ) : null}
+            </div>
           </div>
-        </div>
-      ))}
-    </ul>
-  </section>
+        ))}
+      </ul>
+    </section>
+  )
 }
--- components/skills.tsx
+++ components/skills.tsx
@@ -1,70 +1,90 @@
-import Image from "next/image";
+import Image from "next/image"
 
+export const Skills = ({
+  skillsRef,
+}: {
+  skillsRef: React.MutableRefObject<HTMLElement | null>
+}) => {
+  const confidentWith = [
+    { name: "HTML", image: "/html.png" },
+    { name: "CSS", image: "/css.png" },
+    { name: "JavaScript", image: "/javascript.png" },
+    { name: "TypeScript", image: "/typescript.png" },
+    { name: "React.js", image: "/react.png" },
+    { name: "Next.js", image: "/next.png" },
+    { name: "Tailwind.css", image: "/tailwind.png" },
+    { name: "Supabase", image: "/supabase.png" },
+  ]
 
-export const Skills = ({ skillsRef }: { skillsRef: React.MutableRefObject<HTMLElement | null> }) => {
+  const usedInPast = [
+    { name: "Python", image: "/python.png" },
+    { name: "Flask", image: "/flask.png" },
+    { name: "Solidity", image: "/solidity.png" },
+  ]
 
-	const confidentWith = [
-		{ name: 'HTML', image: '/html.png' },
-		{ name: 'CSS', image: '/css.png' },
-		{ name: 'JavaScript', image: '/javascript.png' },
-		{ name: 'TypeScript', image: '/typescript.png' },
-		{ name: 'React.js', image: '/react.png' },
-		{ name: 'Next.js', image: '/next.png' },
-		{ name: 'Tailwind.css', image: '/tailwind.png' },
-		{ name: 'Supabase', image: '/supabase.png' },
-	]
+  const expertise = [
+    { name: "TypeScript", image: "/typescript.png" },
+    { name: "React.js", image: "/react.png" },
+    { name: "Next.js", image: "/next.png" },
+    { name: "PostgreSQL", image: "/postgresql.png" },
+    { name: "MongoDB", image: "/mongodb.png" },
+    { name: "React Query", image: "/react-query.png" },
+    { name: "Zustand", image: "/zustand.png" },
+    { name: "Redux Tool Kit", image: "/redux.png" },
+    { name: "Jest", image: "/jest.png" },
+    { name: "React Testing Library", image: "/react-testing-library.png" },
+  ]
 
-	const usedInPast = [
-		{ name: 'Python', image: '/python.png' },
-		{ name: 'Flask', image: '/flask.png' },
-		{ name: 'Solidity', image: '/solidity.png' },
+  return (
+    <section
+      className="w-full max-w-7xl mx-auto p-5 min-h-[calc(100vh-64px)] sm:min-h-[calc(100vh-80px)]"
+      ref={skillsRef}
+      id="skills">
+      <div className="my-5 text-xl font-bold">Skills</div>
 
-	]
+      <div className="mt-5 mb-5 text-lg font-bold">I am confident with</div>
+      <ul className="grid grid-cols-2 gap-5 sm:grid-cols-3">
+        {confidentWith.map((item) => (
+          <li key={item.name}>
+            <div className="flex items-center space-x-1">
+              <div>
+                <Image src={item.image} alt={""} height={35} width={35} />
+              </div>
+              <div>{item.name}</div>
+            </div>
+          </li>
+        ))}
+      </ul>
 
-	const expertise = [
-		{ name: 'Next.js', image: '/next.png' },
-		{ name: 'PostgreSQL', image: '/postgresql.png' },
-		{ name: 'MongoDB', image: '/mongodb.png' },
-		{ name: 'Redux Tool Kit', image: '/redux.png' },
-	]
+      <div className="mt-10 mb-5 text-lg font-bold">I have used in past</div>
+      <ul className="grid grid-cols-2 gap-5 sm:grid-cols-3">
+        {usedInPast.map((item) => (
+          <li key={item.name}>
+            <div className="flex items-center space-x-1">
+              <div>
+                <Image src={item.image} alt={""} height={35} width={35} />
+              </div>
+              <div>{item.name}</div>
+            </div>
+          </li>
+        ))}
+      </ul>
 
-	return <section
-		className="w-full max-w-7xl mx-auto p-5 min-h-[calc(100vh-64px)] sm:min-h-[calc(100vh-80px)]"
-		ref={skillsRef}
-		id="skills">
-		<div className="my-5 text-xl font-bold">Skills</div>
-
-
-
-		<div className="mt-5 mb-5 text-lg font-bold">I am confident with</div>
-		<ul className="grid grid-cols-2 gap-5 sm:grid-cols-3">
-			{confidentWith.map((item) => <li key={item.name}>
-				<div className="flex items-center space-x-1">
-					<div><Image src={item.image} alt={""} height={30} width={30} /></div>
-					<div>{item.name}</div>
-				</div>
-			</li>)}
-		</ul>
-
-		<div className="mt-10 mb-5 text-lg font-bold">I have used in past</div>
-		<ul className="grid grid-cols-2 gap-5 sm:grid-cols-3">
-			{usedInPast.map((item) => <li key={item.name}>
-				<div className="flex items-center space-x-1">
-					<div><Image src={item.image} alt={""} height={30} width={30} /></div>
-					<div>{item.name}</div>
-				</div>
-			</li>)}
-		</ul>
-
-		<div className="mt-10 mb-5 text-lg font-bold">I want to learn/gain expertise in near future</div>
-		<ul className="grid grid-cols-2 gap-5 sm:grid-cols-3">
-			{expertise.map((item) => <li key={item.name}>
-				<div className="flex items-center space-x-1">
-					<div><Image src={item.image} alt={""} height={30} width={30} /></div>
-					<div>{item.name}</div>
-				</div>
-			</li>)}
-		</ul>
-
-	</section>
+      <div className="mt-10 mb-5 text-lg font-bold">
+        I want to learn/gain expertise in near future
+      </div>
+      <ul className="grid grid-cols-2 gap-5 sm:grid-cols-3">
+        {expertise.map((item) => (
+          <li key={item.name}>
+            <div className="flex items-center space-x-1">
+              <div>
+                <Image src={item.image} alt={""} height={35} width={35} />
+              </div>
+              <div>{item.name}</div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
 }
