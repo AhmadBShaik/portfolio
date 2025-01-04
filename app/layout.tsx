a03d65ea0c74e09de78579e3864efb2afea17f52
AhmadBShaik <ahmad.shaik1106@gmail.com>
2025-01-04 23:32:53 +0530

    feat: update website metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,48 @@ import { RotationMultiplierContextProvider } from "@/context/rotationContext"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Ahmad B Shaik | Portfolio",
-  description: "Ahmad's personal portfolio website",
-}
+  title: "Ahmad Basha Shaik | Full-Stack Developer & Front-End Specialist",
+  description:
+    "Skilled Full-Stack Developer with a strong focus on creating seamless and engaging front-end user experiences. Passionate about crafting responsive, high-performance web applications using modern technologies.",
+  keywords: [
+    "IT Professional",
+    "Frontend Developer",
+    "Frontend Engineer",
+    "Backend Developer",
+    "Backend Engineer",
+    "Fullstack Developer",
+    "Fullstack Engineer",
+    "Software Developer",
+    "Software Engineer",
+    "Remote Developer",
+    "Remote",
+    "Professional Software Developer",
+    "Web Development Expert",
+    "JavaScript Specialist",
+    "TypeScript Specialist",
+    "UI/UX Developer",
+    "Responsive Web Design",
+    "Web Application Developer"
+  ],
+  authors: [{ name: "Ahmad Basha Shaik", url: "https://ahmadbshaik.com" }],
+  openGraph: {
+    title: "Ahmad Basha Shaik | IT Professional",
+    description:
+      "Skilled Full-Stack Developer with a strong focus on creating seamless and engaging front-end user experiences. Passionate about crafting responsive, high-performance web applications using modern technologies.",
+    url: "https://ahmadbshaik.com",
+    siteName: "AhmadBShaik | Portfolio",
+    images: [
+      {
+        url: "/website-thumbnail.png",
+        width: 1200,
+        height: 630,
+        alt: "AhmadBShaik | Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  }
+};
 
 export default function RootLayout({
   children,
