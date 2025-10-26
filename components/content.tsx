08acb1fae14398d59b458f63a038856e6f395552
AhmadBShaik <ahmad.shaik1106@gmail.com>
2025-10-26 15:55:39 +0530

    remove contact email

--- components/content.tsx
+++ components/content.tsx
@@ -1,67 +1,68 @@
-"use client"
-import React, { useEffect, useRef, useState } from "react"
-import { useSectionContext } from "@/context/sectionContext"
-import { useIsInViewport } from "@/hooks/viewport"
-import { Hero } from "./hero"
-import { AboutMe } from "./about-me"
-import { Skills } from "./skills"
-import { Projects } from "./projects"
-import { Contact } from "./contact"
-import { useRotationMultiplierContext } from "@/context/rotationContext"
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import { useSectionContext } from "@/context/sectionContext";
+import { useIsInViewport } from "@/hooks/viewport";
+import { Hero } from "./hero";
+import { AboutMe } from "./about-me";
+import { Skills } from "./skills";
+import { Projects } from "./projects";
+// import { Contact } from "./contact";
+import { useRotationMultiplierContext } from "@/context/rotationContext";
 
 const Content = () => {
-  const { sectionName, setSectionName } = useSectionContext()
-  const { setRotationMultiplier } = useRotationMultiplierContext()
+  const { sectionName, setSectionName } = useSectionContext();
+  const { setRotationMultiplier } = useRotationMultiplierContext();
 
-  const homeRef = useRef<HTMLElement | null>(null)
-  const aboutRef = useRef<HTMLElement | null>(null)
-  const skillsRef = useRef<HTMLElement | null>(null)
-  const projectsRef = useRef<HTMLElement | null>(null)
-  const contactRef = useRef<HTMLElement | null>(null)
+  const homeRef = useRef<HTMLElement | null>(null);
+  const aboutRef = useRef<HTMLElement | null>(null);
+  const skillsRef = useRef<HTMLElement | null>(null);
+  const projectsRef = useRef<HTMLElement | null>(null);
+  // const contactRef = useRef<HTMLElement | null>(null);
 
   const isInViewportHome = useIsInViewport(
-    homeRef as React.MutableRefObject<HTMLElement>,
-  )
+    homeRef as React.MutableRefObject<HTMLElement>
+  );
 
   const isInViewportAbout = useIsInViewport(
-    aboutRef as React.MutableRefObject<HTMLElement>,
-  )
+    aboutRef as React.MutableRefObject<HTMLElement>
+  );
 
   const isInViewportSkills = useIsInViewport(
-    skillsRef as React.MutableRefObject<HTMLElement>,
-  )
+    skillsRef as React.MutableRefObject<HTMLElement>
+  );
 
   const isInViewportProjects = useIsInViewport(
-    projectsRef as React.MutableRefObject<HTMLElement>,
-  )
+    projectsRef as React.MutableRefObject<HTMLElement>
+  );
 
-  const isInViewportContact = useIsInViewport(
-    contactRef as React.MutableRefObject<HTMLElement>,
-  )
+  // const isInViewportContact = useIsInViewport(
+  //   contactRef as React.MutableRefObject<HTMLElement>
+  // );
 
   useEffect(() => {
     if (isInViewportHome) {
-      setSectionName("")
+      setSectionName("");
     } else if (isInViewportAbout) {
-      setSectionName("/About")
+      setSectionName("/About");
     } else if (isInViewportSkills) {
-      setSectionName("/Skills")
+      setSectionName("/Skills");
     } else if (isInViewportProjects) {
-      setSectionName("/Projects")
-    } else if (isInViewportContact) {
-      setSectionName("/Contact")
+      setSectionName("/Projects");
     }
+    // else if (isInViewportContact) {
+    //   setSectionName("/Contact");
+    // }
   }, [
     isInViewportHome,
     isInViewportAbout,
     isInViewportSkills,
     isInViewportProjects,
-    isInViewportContact,
-  ])
+    // isInViewportContact,
+  ]);
 
   useEffect(() => {
-    setRotationMultiplier((prev) => prev + 120)
-  }, [sectionName])
+    setRotationMultiplier((prev) => prev + 120);
+  }, [sectionName]);
 
   return (
     <section className="flex flex-col xl:px-0 mx-auto">
@@ -69,9 +70,9 @@ const Content = () => {
       <AboutMe aboutRef={aboutRef} />
       <Skills skillsRef={skillsRef} />
       <Projects projectsRef={projectsRef} />
-      <Contact contactRef={contactRef} />
+      {/* <Contact contactRef={contactRef} /> */}
     </section>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
